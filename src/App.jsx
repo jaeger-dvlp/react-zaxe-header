71d3f1fe4ea97e55949e72eac0204942ea5fb8d8
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-03 10:31:39 +0300

    Import Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './Components/Header';
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
